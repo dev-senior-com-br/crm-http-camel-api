61b46338b0f13731c24a2a761f5c23fccb063ed9
Leonardo Cardoso <leonardo.cardoso@senior.com.br>
2022-04-26 16:04:02 -0300

    Implementar service PrepareParameters

---
+++ src/main/java/br/com/senior/crm/http/camel/services/PrepareParameters.java
@@ -0,0 +1,30 @@
+package br.com.senior.crm.http.camel.services;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+
+import java.util.Base64;
+import java.util.List;
+
+public class PrepareParameters
+{
+    private static final String HEADER_PARAMS = "X-Param-";
+
+    public static void prepare(Exchange exchange, List<String> params)
+    {
+        Message message = exchange.getMessage();
+
+        params.forEach(param -> {
+            String paramComplete = HEADER_PARAMS + param;
+            if (message.getHeader(paramComplete) != null) {
+                exchange.setProperty(param, decode(message.getHeader(paramComplete).toString()));
+            }
+        });
+    }
+
+    private static String decode(String valueEncoded)
+    {
+        byte[] valueDecoded = Base64.getDecoder().decode(valueEncoded);
+        return new String(valueDecoded);
+    }
+}
