c4be8631fa0749ed010175ad1aa9631300d68fa5
Lucas Nunes <lucas.nunes@senior.com.br>
2022-06-20 08:44:23 -0300

    Adicionando properties para validação

--- src/main/java/br/com/senior/crm/http/camel/services/impl/FilterFields.java
+++ src/main/java/br/com/senior/crm/http/camel/services/impl/FilterFields.java
@@ -1,6 +1,5 @@
 package br.com.senior.crm.http.camel.services.impl;
 
-import br.com.senior.crm.http.camel.entities.account.Account;
 import br.com.senior.crm.http.camel.entities.account.AccountDefinition;
 import br.com.senior.crm.http.camel.entities.account.AccountDefinitionCollection;
 import br.com.senior.crm.http.camel.utils.constants.AccountParamsConstant;
@@ -18,58 +17,53 @@ import java.util.UUID;
 public class FilterFields {
     @NonNull
     private RouteBuilder builder;
-    private final UUID id = UUID.randomUUID();
+    private final String id = UUID.randomUUID().toString();
     @Getter
-    private final String directImpl = "direct:crm-filter-fields-" + id.toString();
+    private final String directImpl = "direct:crm-filter-fields-".concat(id);
     @Getter
-    private final String directResponse = "direct:crm-filter-fields-response-" + id.toString();
+    private final String directResponse = "direct:crm-filter-fields-response-".concat(id);
 
-    private static String DIRECT_END_REST = "direct:crm-filter-fields-end-rest";
+    //private static String DIRECT_END_REST = "direct:crm-filter-fields-end-rest";
 
-    public void prepare()
-    {
+    public static final String PROPERTY_FILTER_SUCCESS = "property:filter-fields-is-valid";
+
+    public void prepare() {
         builder
             .from(directImpl)
-            .choice()
-            .when(this::filter)
-                .to(directResponse)
-            .otherwise()
-                .to(DIRECT_END_REST)
-            .endChoice()
+            .process(exchange -> exchange.setProperty(PROPERTY_FILTER_SUCCESS, filter(exchange)))
         ;
 
-        builder.
-            from(DIRECT_END_REST)
-            .log("Não passou no filtro")
-            .process(exchange -> exchange.getMessage().setBody(null))
-            .endRest()
-        ;
+//        builder.
+//            from(DIRECT_END_REST)
+//            .log("Não passou no filtro")
+//            .process(exchange -> exchange.getMessage().setBody(null))
+//            .endRest()
+//        ;
     }
 
-    private boolean filter(Exchange exchange)
-    {
-        boolean isValid = false;
-
-        if (exchange.getMessage().getBody() instanceof AccountDefinitionCollection) {
-            isValid = this.filterAccountDefinition(exchange);
-        }
-
-        return isValid;
+    private Boolean filter(Exchange exchange) {//
+        return exchange.getMessage().getBody() instanceof AccountDefinitionCollection//
+            ? this.filterAccountDefinition(exchange)//
+            : Boolean.FALSE;//
     }
 
-    private boolean filterAccountDefinition(Exchange exchange)
-    {
-        AccountDefinitionCollection collection = exchange.getMessage().getBody(AccountDefinitionCollection.class);
-        Long typeAccount = exchange.getProperty(AccountParamsConstant.TYPE_ACCOUNT, Long.class);
-        boolean isValid = false;
+    private Boolean filterAccountDefinition(Exchange exchange) {
+        AccountDefinitionCollection collection = exchange.getMessage().getBody(AccountDefinitionCollection.class);//
+        Long typeAccount = exchange.getProperty(AccountParamsConstant.TYPE_ACCOUNT, Long.class);//
+        Boolean isValid = false;//
 
-        for (AccountDefinition accountDefinition : collection.definitions) {
-            if (!isValid) {
-                isValid = accountDefinition.getAccountType().getId().equals(typeAccount);
-                log.info("Comparative between account type: Definition " + accountDefinition.getId() + " with type account " + accountDefinition.getAccountType().getId() + " and parameter type account " + typeAccount);
-            }
-        }
+        for (AccountDefinition accountDefinition : collection.definitions) {//
+            if (isValid.equals(Boolean.FALSE)) {//
+                isValid = accountDefinition.getAccountType().getId().equals(typeAccount);//
+                log.info(//
+                    "Comparative between account type: Definition {} with type account {} and parameter type account {}",//
+                    accountDefinition.getId(),//
+                    accountDefinition.getAccountType().getId(),//
+                    typeAccount//
+                );//
+            }//
+        }//
 
-        return isValid;
+        return isValid;//
     }
 }
